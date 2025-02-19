614859b7e3c2571403a2301f79b80e0b1c111276
Nahid44332 <nahidhossen3278@gmail.com>
2025-02-18 23:37:07 -0800

    js Function

---
+++ script.js
@@ -0,0 +1,46 @@
+function sum(Number1, Number2)
+ {
+    let x = Number1;
+    let y = Number2;
+
+    let result = Number1+Number2;
+
+    console.log("The sum is="+result);
+}
+
+function sub(number1, number2)
+{
+    let digit1 = number1;
+    let digit2 = number2;
+
+    let result = number1-number2;
+
+    console.log("The Sub is="+result);
+}
+
+function mul(Multi1, Multi2)
+{
+    let digit3 = Multi1;
+    let digit4 = Multi2;
+
+    let result = Multi1*Multi2;
+
+    console.log("The Multiplication is="+result);
+}
+
+function Division(div1, div2)
+{
+    let digit5 = div1;
+    let digit6 = div2;
+
+    let result = div1/div2;
+
+    console.log("The Division is="+result);
+}
+sum(40,50);
+
+sub(60,30);
+
+mul(100,10);
+
+Division(100,5);
